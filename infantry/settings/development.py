4c39f4d13fbc82d6fb8ce334bfebdc20f0176293
Dariusz Suszczewicz <dariusz.suszczewicz@gmail.com>
2017-12-15 23:00:56 +0100

    added settings for allowed hosts and logging

--- infantry/settings/development.py
+++ infantry/settings/development.py
@@ -2,6 +2,24 @@ from infantry.settings.base import *
 
 DEBUG = True
 
+ALLOWED_HOSTS += ["*"]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 try:
     from .local import *
 except ImportError:
