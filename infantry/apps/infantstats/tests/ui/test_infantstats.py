9a9275fcac2e50d2e1734c736aeaac14be3f8068
Dariusz Suszczewicz <dariusz.suszczewicz@gmail.com>
2017-12-17 12:35:42 +0100

    rewrote existing tests to use pytest

--- infantry/apps/infantstats/tests/ui/test_infantstats.py
+++ infantry/apps/infantstats/tests/ui/test_infantstats.py
@@ -1,19 +1,15 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import pytest
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 
-class InfantStatsUITests(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.browser = WebDriver()
+@pytest.fixture
+def browser():
+    driver = WebDriver()
+    yield driver
+    driver.quit()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.browser.quit()
-        super().tearDownClass()
 
-    def test__access_the_statistics_page__should_return_200(self):
-        self.browser.get(f'{self.live_server_url}/statistics')
-        self.assertEqual(self.browser.title, "Statistics")
-
+@pytest.mark.ui
+def test_access_statistics_page(live_server, browser):
+    browser.get(f'{live_server.url}/statistics')
+    assert browser.title == "Statistics"
--- infantry/apps/infantstats/tests/unit/test_views.py
+++ infantry/apps/infantstats/tests/unit/test_views.py
@@ -1,12 +1,11 @@
-from django.test import TestCase
+import pytest
 from django.urls import reverse
 
 from infantry.apps.infantstats import urls
 
 
-class StatisticsViewsTests(TestCase):
-    def test__statistics_home_page_should_return_200_when_accessed(self):
-        response = self.client.get(reverse(urls.base_url))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "base.html")
-
+@pytest.mark.django_db
+def test_statistics_page_returns_200(client):
+    response = client.get(reverse(urls.base_url))
+    assert response.status_code == 200
+    assert "base.html" in [template.name for template in response.templates]
