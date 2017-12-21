ca5c57fda33a48a3152dd00a0b067519b2c41a06
Dariusz Suszczewicz <dariusz.suszczewicz@gmail.com>
2017-12-21 17:28:34 +0100

    added source sqlite database upload view and urls conf

--- infantry/apps/infantstats/urls.py
+++ infantry/apps/infantstats/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from infantry.apps.infantstats import views
 
 base_url = "statistics-base-url"
+upload_url = "statistics-upload-url"
 
 urlpatterns = [
-    path("", views.basic_stats, name=base_url)
+    path("", views.basic_stats, name=base_url),
+    path("upload", views.upload, name=upload_url)
 ]
--- infantry/apps/infantstats/views.py
+++ infantry/apps/infantstats/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from . import forms
+from . import urls
+
 
 def basic_stats(request):
-    return HttpResponse(render(request, "base.html"))
+    return render(request, "base.html")
+
+
+def upload(request):
+    assert isinstance(request, HttpRequest)
+    if request.method == "POST":
+        form = forms.UploadForm(request.POST, request.FILES)
+        if not form.is_valid():
+            return render(request, "upload.html", context={"form": form})
+        else:
+            return HttpResponseRedirect(reverse(urls.base_url))
+    return render(request, "upload.html", context={"form": forms.UploadForm()})
